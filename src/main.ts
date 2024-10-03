29e1defc26b85874d55211c14f44e32ef15b139f
Varga-Tóth Máté <vargatothmate@gmail.com>
2024-10-03 16:32:46 +0200

    added custom colors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,49 @@
-import './assets/main.css'
+// main.ts
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-createApp(App).mount('#app')
+const myCustomLightTheme = {
+  dark: false,
+  colors: {
+    mybrown: "#8B4513",
+    white: "#FEF9F3",
+  },
+  variables: {
+    "border-color": "#000000",
+    "border-opacity": 0.12,
+  },
+};
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: "myCustomLightTheme",
+    themes: {
+      myCustomLightTheme,
+    },
+  },
+});
+
+// const vuetify = createVuetify({
+//   components,
+//   directives,
+//   theme: {
+//     defaultTheme: "myCustomLightTheme",
+//     themes: {
+//       myCustomLightTheme: {
+//         dark: false,
+//         colors: {
+//           background: "#8B4513", // Example global background color
+//         },
+//       },
+//     },
+//   },
+// });
+
+createApp(App).use(vuetify).mount("#app");
